6167878046080e50e3824d2c7caacb3f324165f0
DaYoung-woo <131967254+DaYoung-woo@users.noreply.github.com>
2024-06-20 15:24:41 +0900

    내보내기, git 저장 기능 추가

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BigPlayButton, ControlBar, LoadingSpinner, Player } from "video-react";
 import 'video-react/dist/video-react.css'
-const VidoePlayer = ({src}) => {
+const VidoePlayer = ({src, onPlayerChange = () => {}, onChange = () => {}, startTime = undefined}) => {
   const [player, setPlayer] = useState()
   const [source, setSource] = useState()
+  const [playerState, setPlayerState] = useState(undefined);
 
+  useEffect(() => {
+    setSource(URL.createObjectURL(src))
+  }, [src])
+
+  useEffect(() => {
+    if(playerState) onChange(playerState)
+  }, [playerState])
+
+  useEffect(() => {
+    onPlayerChange(player)
+    if(player) player.subscribeToStateChange(setPlayerState)
+  }, [player])
+  
   return (  
     <div className={'video-player'}>
       <Player
@@ -12,6 +26,7 @@ const VidoePlayer = ({src}) => {
           setPlayer(player)
         }}
         src={source}
+        startTime={startTime}
       >
         <source src={source} />
         <BigPlayButton position="center"/>
--- src/pages/VideoEditor.jsx
+++ src/pages/VideoEditor.jsx
@@ -1,12 +1,13 @@
 import { useState, useRef, useEffect } from 'react';
-import { Button } from 'react-bootstrap'
+import { Button, Modal, Spinner, Toast, ToastContainer } from 'react-bootstrap';
 import { createFFmpeg } from '@ffmpeg/ffmpeg'
 import styles from './VideoEditor.module.css'
 
 import video_placeholder from '../assets/images/video_placeholder.png'
-import VidoePlayer from './VideoPlayer';
+import VidoePlayer from '../components/VideoPlayer';
 import MultiRangeSlider from '../components/MultiRangeSlider';
-import VideoConversionButton from './VideoConversionButton';
+import VideoConversionButton from '../components/VideoConversionButton';
+import { sliderValueToVideoTime } from '../utils/utils';
 
 const ffmpeg = createFFmpeg({
   log: true
@@ -16,7 +17,10 @@ const VideoEditor = () => {
   const [videoFile, setVideoFile]  = useState();
   const [ffmpegLoaded, setFfmpegLoaded] = useState(false);
   const [sliderValues, setSliderValues] = useState([0, 100])
-  
+  const [videoPlayerState, setVideoPlayerState] = useState()
+  const [videoPlayer, setVideoPlayer] = useState()
+  const [processing, setProcessing] = useState(false)
+  const [show, setShow] = useState(false)
 
   useEffect(() => {
     ffmpeg.load().then(() => {
@@ -24,7 +28,36 @@ const VideoEditor = () => {
     })
   }, [])
 
+  useEffect(() => {
+    const min = sliderValues[0]
+
+    if(min !== undefined && videoPlayerState && videoPlayer) {
+      videoPlayer.seek(sliderValueToVideoTime(videoPlayerState.duration, min))
+    }
+  }, [sliderValues])
+
+  useEffect(() => {
+    if(videoPlayer && videoPlayerState) {
+      const [min, max] = sliderValues;
+
+      const minTime = sliderValueToVideoTime(videoPlayerState.duration, min)
+      const maxTime = sliderValueToVideoTime(videoPlayerState.duration, max)
+
+      if(videoPlayerState.currentTime < minTime) {
+        videoPlayer.seek(minTime)
+      }
+      if(videoPlayerState.currentTime > maxTime) {
+        videoPlayer.seek(minTime)
+      }
+    }
+  }, [videoPlayerState])
+
+  useEffect(() => {
+    setVideoPlayerState(undefined)
+  },[videoFile])
+
   if(!ffmpegLoaded) return <div>Loading...</div>
+
   return (
     <article className='layout' style={{padding: '56px 16px'}}>
       <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 16}}>
@@ -32,7 +65,13 @@ const VideoEditor = () => {
         {
           videoFile && (
             <div>
-              <input type="file" accept='video/*' style={{display: 'none'}} ref={uploadFile}/>
+              <input 
+                onChange={(e) => setVideoFile(e.target.files[0])}
+                type="file" 
+                accept='video/*' 
+                style={{display: 'none'}} 
+                ref={uploadFile}
+              />
               <Button className={styles.re__upload__btn} style={{width: 'fit-content'}} onClick={() => uploadFile.current.click()}>
                 비디오 재선택
               </Button>
@@ -43,12 +82,20 @@ const VideoEditor = () => {
       <section>
         {
           videoFile ? (
-            <VidoePlayer />
+            <VidoePlayer 
+              src={videoFile} 
+              onPlayerChange={(videoPlayer) => {
+                setVideoPlayer(videoPlayer)
+              }}
+              onChange={(videoPlayerState) => {
+                setVideoPlayerState(videoPlayerState)
+              }}
+            />
           ): (
             <>
               <img src={video_placeholder} alt="비디오를 업로드해주세요" style={{marginBottom: 32}}/>
               <div>
-                <input type="file" accept='video/*' style={{display: 'none'}} ref={uploadFile}/>
+                <input type="file" accept='video/*' style={{display: 'none'}} ref={uploadFile} onChange={(e) => setVideoFile(e.target.files[0])}/>
                 <Button className={styles.upload__btn} onClick={() => uploadFile.current.click()}>
                   비디오 업로드하기
                 </Button>
@@ -62,14 +109,52 @@ const VideoEditor = () => {
         {videoFile && (
           <>
           <section style={{width: "100%", marginTop: 30, marginBottom: 62, display: 'flex', justifyContent: 'center'}}>
-            <MultiRangeSlider min={0} max={100} onChange={(min, max) => {setSliderValues([min, max])}}/>
+            <MultiRangeSlider min={0} max={100} onChange={({min, max}) => {setSliderValues([min, max])}}/>
           </section>
           <section>
-            <VideoConversionButton/>
+            <VideoConversionButton
+              onConversionStart={() => {
+                setProcessing(true)
+              }}
+              onConversionEnd={() => {
+                setProcessing(false)
+              }}
+              ffmpeg={ffmpeg}
+              videoPlayerState={videoPlayerState}
+              sliderValues={sliderValues}
+              videoFile={videoFile}
+            />
           </section>
           </>
         )}
-      
+
+        <ToastContainer className="p-3" position={'top-center'} style={{ zIndex: 1 }}>
+          <Toast onClose={() => setShow(false)} show={show} delay={2000} bg="dark" autohide>
+              <Toast.Header closeButton={false}>
+                  <strong className="me-auto">Video Editor</strong>
+              </Toast.Header>
+              <Toast.Body>내보내기가 완료되었습니다.</Toast.Body>
+          </Toast>
+      </ToastContainer>
+
+      <Modal
+          show={processing}
+          onHide={() => setProcessing(false)}
+          backdrop={false}
+          keyboard={false}
+          centered
+          size="sm"
+      >
+          <div style={{ textAlign: 'center' }}>
+              <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+              </Spinner>
+
+              <p style={{ marginTop: 16, fontSize: 14, fontWeight: 600, color: '#c8c8c8' }}>
+                  내보내기가 진행중입니다.
+              </p>
+          </div>
+      </Modal>
     </article>
   );
 }
